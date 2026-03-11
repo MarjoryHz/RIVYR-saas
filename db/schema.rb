2db64b3a32bab96a2e2fc8dec234f1da5bd6be19
Marjory Hiez <marjory@rivyrgroup.com>
2026-03-11 10:43:46 +0100

    navbar add

--- db/schema.rb
+++ db/schema.rb
@@ -185,13 +185,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_142641) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
-    t.string "status"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
